a6d7e065a2f4bda4efb275cf3c77b90995880c06
Kyle Coberly <kyle.coberly@gmail.com>
2022-04-27 18:06:00 -0600

    Remove tests, add solution

--- anagram.ts
+++ anagram.ts
@@ -1,5 +1,23 @@
-export class Anagram {
-  constructor() {}
+function sort(input: string): string {
+  return input.toString().toLowerCase().split("").sort().join("");
+}
 
-  public matches() {}
+export class Anagram {
+  private value: string;
+  constructor(input: string) {
+    this.value = input;
+  }
+
+  public matches(...input: string[]): string[] {
+    const result: string[] = [];
+    for (const each of input) {
+      if (sort(each) === sort(this.value)) {
+        if (each.toLowerCase() === this.value.toLowerCase()) {
+          continue;
+        }
+        result.push(each);
+      }
+    }
+    return result;
+  }
 }
